75eefdac5bce5cf6967663ac2d9d8f8137e17010
Craig Buchanan <craig.buchanan@cogniza.com>
2021-10-28 11:20:32 -0500

    version 0.0.1
    dot-sourcing all public and private functions

---
+++ PsZebra/PsZebra.psm1
@@ -0,0 +1,10 @@
+#
+# load (dot-source) *.PS1 files, excluding unit-test scripts (*.Tests.*), and disabled scripts (__*)
+#
+
+@("$PSScriptRoot\Public\*.ps1","$PSScriptRoot\Private\*.ps1") | Get-ChildItem -ErrorAction 'Continue' | 
+    Where-Object { $_.Name -like '*.ps1' -and $_.Name -notlike '__*' -and $_.Name -notlike '*.Tests*' } | 
+    ForEach-Object {
+        # dot-source script
+        . $_
+    }
